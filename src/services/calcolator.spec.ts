d6795dfb9f0b85a4f9ea2e3b28ab2a54c93a03b1
Iván Tajes <tajespasarela@gmail.com>
2023-03-21 09:36:19 +0100

    fix CalculatorService tests

--- src/services/calcolator.spec.ts
+++ src/services/calcolator.spec.ts
@@ -1,21 +1,20 @@
+import {
+  additionOperation,
+  divisionOperation,
+  multiplicationOperation
+} from '@/domain/binary-operations';
+import type { Numeric } from '@/domain/entities';
+import { number2, number3, number5, number9 } from '@/domain/numerics';
 import { describe, it, expect } from 'vitest';
 import { CalculatorService } from './calculator.service';
-import {
-  Numeric,
-  additionOperation,
-  multiplicationOperation,
-  number2,
-  number3,
-  number5
-} from './operations';
 
 describe('Testing calculatorMachine', () => {
   it('Keeps the operators precedence', () => {
-    const calculator = new CalculatorService();
+    const calculatorService = new CalculatorService();
 
     expect(
       (
-        calculator.resolve([
+        calculatorService.resolve([
           number2,
           additionOperation,
           number5,
@@ -25,8 +24,32 @@ describe('Testing calculatorMachine', () => {
       ).value
     ).toBe(17);
 
-    //expect(calculator.resolve([2, '*', 5, '+', 9, '/', 3])).toBe(13);
+    expect(
+      (
+        calculatorService.resolve([
+          number2,
+          multiplicationOperation,
+          number5,
+          additionOperation,
+          number9,
+          divisionOperation,
+          number3
+        ])[0] as Numeric
+      ).value
+    ).toBe(13);
 
-    //expect(calculator.resolve([2, '+', 5, '*', 9, '/', 3])).toBe(17);
+    expect(
+      (
+        calculatorService.resolve([
+          number2,
+          additionOperation,
+          number5,
+          multiplicationOperation,
+          number9,
+          divisionOperation,
+          number3
+        ])[0] as Numeric
+      ).value
+    ).toBe(17);
   });
 });
