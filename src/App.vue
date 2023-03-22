de656e81e052a15d0160c08521f90eeb66489c03
Iván Tajes <tajespasarela@gmail.com>
2023-03-22 16:31:54 +0100

    Adding dist

--- src/App.vue
+++
@@ -1,8 +0,0 @@
-<script setup lang="ts">
-import Calculator from './components/Calculator.vue';
-import './styles/index.pcss';
-</script>
-
-<template>
-  <Calculator class="mx-auto mt-[15vh]" msg="You did it!" />
-</template>
